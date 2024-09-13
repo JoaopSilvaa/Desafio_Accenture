29220e189711c3241b23481852f8a05521b94d95
JoaopSilvaa <joaopsilva.dev@gmail.com>
2024-09-13 12:29:18 -0300

    fix: Correção de salvamento de dados posterior, por motivo de entidade empresa não estar salvando dados

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
 import {EmpresaComponent} from "./empresa/empresa.component";
 import {FornecedorComponent} from "./fornecedor/fornecedor.component";
-import {AppComponent} from "./app.component";
 
 export const routes: Routes = [
 
-  { path: '', component: AppComponent },
+  { path: '', component: EmpresaComponent },
   { path: 'empresa', component: EmpresaComponent },
   { path: 'fornecedor', component: FornecedorComponent },
   { path: '**', redirectTo: '' }
